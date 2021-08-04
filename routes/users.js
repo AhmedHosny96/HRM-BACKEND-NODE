973023b0e6df742d9892c01c96ef45e118c0c759
Hosny <aahosny1@gmail.com>
2021-08-04 18:24:50 +0300

    added user api requests with jwt generation

--- routes/users.js
+++ routes/users.js
@@ -12,27 +12,26 @@ router.get("/", async (req, res) => {
 // create new user
 
 router.post("/", async (req, res) => {
-  //validate
+  //validate inputs
   const { error } = Validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
-  // check if user already exits
+  // check if user already exists
   let user = await User.findOne({ email: req.body.email });
-  if (user) return res.status(400).send("User Already Registered");
-
-  // create the user
-  user = new User(_.pick(req.body, ["name", "email", "password"]));
-  // salt the password
-  const salt = await bcrypt.genSalt(10); // bcrypt is promise therefore await
-  // hash the password with the salt
+  if (user) return res.status(400).send("User Already exists");
+  // create user object by picking using lodash
+  user = User(_.pick(req.body, ["name", "email", "password"]));
+  // salt the password using bcrypt
+  const salt = await bcrypt.genSalt(10); //bcrypt returns promise therefore await
+  // hash the password using the salt
   user.password = await bcrypt.hash(user.password, salt);
   // save the user to db
   await user.save();
-  //generate token
-
+  // generate the auth token
   const token = user.generateAuthToken();
+  // setting the response headers
   res
     .header("x-auth-token", token)
-    .send(_.pick(user, ["_id ", "name", "email"]));
+    .send(_.pick(user, ["_id", "name", "email"]));
 });
 
 module.exports = router;
--- startup/routes.js
+++ startup/routes.js
@@ -2,10 +2,12 @@ const express = require("express");
 const branches = require("../routes/branches");
 const employees = require("../routes/employees");
 const users = require("../routes/users");
+const login = require("../routes/login");
 
 module.exports = function (app) {
   app.use(express.json());
   app.use("/api/branches", branches);
   app.use("/api/employees", employees);
   app.use("/api/users", users);
+  app.use("/api/login", login);
 };
